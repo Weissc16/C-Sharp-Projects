d9aa83a4ebf15475cf1f0c58e8400cb107c8bf56
Cody Weiss <weissc16@gmail.com>
2023-07-25 20:35:19 -0600

    Updated Branch Assignment

--- Basic_C#_Programs/BranchingAssignment/BranchingAssignment/Program.cs
+++ Basic_C#_Programs/BranchingAssignment/BranchingAssignment/Program.cs
@@ -15,30 +15,32 @@ namespace BranchingAssignment
             Console.Write("What is the weight of your package in lbs? ");
             double packageWeight = Convert.ToDouble(Console.ReadLine());
 
-            Console.Write("What is the width of your package in inches? ");
-            double packageWidth = Convert.ToDouble(Console.ReadLine());
-
-            Console.Write("What is the length of your package in inches? ");
-            double packageLength = Convert.ToDouble(Console.ReadLine());
-
-            Console.Write("What is the height of your package in inches? ");
-            double packageHeight = Convert.ToDouble(Console.ReadLine());
-
-            double quote = ((packageHeight * packageLength * packageWidth) * packageWeight) / 100.00;
-
             if (packageWeight > 50)
             {
                 Console.Write("Package too heavy to be shipped via Package Express.  Have a good day!");
             }
-            else if ((packageWidth + packageLength + packageHeight) > 50)
-            {
-                Console.Write("Package too big to be shipped via Package Express.");
-            }
             else
             {
-                Console.WriteLine("Your quote for this package is:  $ " + quote);
-            }
+                Console.Write("What is the width of your package in inches? ");
+                double packageWidth = Convert.ToDouble(Console.ReadLine());
 
+                Console.Write("What is the length of your package in inches? ");
+                double packageLength = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("What is the height of your package in inches? ");
+                double packageHeight = Convert.ToDouble(Console.ReadLine());
+
+                if ((packageWidth + packageLength + packageHeight) > 50)
+                {
+                    Console.Write("Package too big to be shipped via Package Express.");
+                }
+                else
+                {
+                    double quote = ((packageHeight * packageLength * packageWidth) * packageWeight) / 100.00;
+                    Console.WriteLine("Your quote for this package is:  $ " + quote);
+                }
+
+            }
             Console.ReadLine();
         }
     }
