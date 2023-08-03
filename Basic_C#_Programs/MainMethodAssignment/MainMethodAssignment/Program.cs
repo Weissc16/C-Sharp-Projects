aac0297b0cc6731945d43e178dadb036e92e3e7c
Cody Weiss <weissc16@gmail.com>
2023-08-03 05:40:33 -0600

    Main Method Assignment

---
+++ Basic_C#_Programs/MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myClass newMethod = new myClass();
+
+            Console.WriteLine(newMethod.addition(5));
+            Console.ReadLine();
+
+            myClass subMethod = new myClass();
+
+            Console.WriteLine(subMethod.subtraction((decimal)2.31111323));
+            Console.ReadLine();
+
+            myClass multMethod = new myClass();
+
+            Console.WriteLine(multMethod.multiplication("3"));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Basic_C#_Programs/MainMethodAssignment/MainMethodAssignment/myClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class myClass
+    {
+        public int addition(int num)
+        {
+            return num + 5;
+        }
+        public decimal subtraction(decimal num2)
+        {
+            decimal v = num2 - 1;
+            return Convert.ToInt32(v);            
+        }
+
+        public int multiplication(string num3)
+        {
+            string x = num3;
+            int y = Convert.ToInt32(x);
+            return y * 4;
+
+        }
+    }
+}
