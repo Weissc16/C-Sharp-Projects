1ab413c239dc7b0ff46b8ae3d2117e688fc84e89
Cody Weiss <weissc16@gmail.com>
2023-08-03 10:10:27 -0600

    Method Assignment 3

---
+++ Basic_C#_Programs/MethodAssignment3/MethodAssignment3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myClass newMethod = new myClass();
+
+            newMethod.myMethod(x: 5, y: 7);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Basic_C#_Programs/MethodAssignment3/MethodAssignment3/myClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment3
+{
+    public class myClass
+    {
+        public void myMethod(int x, int y)
+        {
+            x *= 2;
+            Console.WriteLine(y);
+            return;
+        }
+    }
+}
