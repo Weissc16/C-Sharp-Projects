09561ebb1fd107fa534a98a909261726e87ad318
Cody Weiss <weissc16@gmail.com>
2023-08-02 18:31:45 -0600

    Calling Method Update

---
+++ Basic_C#_Programs/CallingMethodAssignment/CallingMethodAssignment/MathMethod.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodAssignment
+{
+    class MathMethod
+    {
+        public int mathMethod1(int num)
+        {
+            return num + 5;
+        }
+
+        public int mathMethod2 (int num)
+        {
+            return num * 7;
+        }
+        public int mathMethod3(int num)
+        {
+            return num - 3;
+        }
+    }
+}
--- Basic_C#_Programs/CallingMethodAssignment/CallingMethodAssignment/Program.cs
+++ Basic_C#_Programs/CallingMethodAssignment/CallingMethodAssignment/Program.cs
@@ -10,29 +10,15 @@ namespace CallingMethodAssignment
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What number would you like to use?");
-           int userNum = Convert.ToInt32(Console.ReadLine());
+            MathMethod myMethod = new MathMethod();
 
-            Multiply numOne = new Multiply();
-            numOne.multiOne = 7;
-            numOne.multiTwo = userNum;
+            Console.WriteLine("Input a number you would like to do some math operations on");
+            int userNum = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("7 times " + userNum + " equals: " + (numOne.multiOne * numOne.multiTwo));
-            Console.ReadLine();
+            Console.WriteLine(myMethod.mathMethod1(userNum));
+            Console.WriteLine(myMethod.mathMethod2(userNum));
+            Console.WriteLine(myMethod.mathMethod3(userNum));
 
-            
-            Subtract numTwo = new Subtract();
-            numTwo.subOne = 6;
-            numTwo.subTwo = userNum;
-
-            Console.WriteLine(userNum + "minus 6 equals: " + (numTwo.subTwo - numTwo.subOne));
-            Console.ReadLine();
-
-            Divide numThree = new Divide();
-            numThree.divOne = 4;
-            numThree.divTwo = userNum;
-
-            Console.WriteLine( userNum + " divided by 4 equals: " + (numThree.divTwo / numThree.divOne));
             Console.ReadLine();
 
         }
