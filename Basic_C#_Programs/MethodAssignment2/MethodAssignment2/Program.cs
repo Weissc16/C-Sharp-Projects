02eca8d84a7fc5d2366e48a1cdfff219445f04fe
Cody Weiss <weissc16@gmail.com>
2023-08-03 09:35:04 -0600

    Main Method Assignment

---
+++ Basic_C#_Programs/MethodAssignment2/MethodAssignment2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClassInt myMethod = new MyClassInt();
+            Console.WriteLine("Input a number you'd like to add");
+            int userValue = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Insert a second optional number to add");
+            int result;
+            if (int.TryParse(Console.ReadLine(), out int userValue2))
+            {
+                result = myMethod.IntClass(userValue, userValue2);
+            }
+            else
+            {
+                result = myMethod.IntClass(userValue);
+            }
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+            
+        }
+    }
+}
---
+++ Basic_C#_Programs/MethodAssignment2/MethodAssignment2/myClassInt.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment2
+{
+    class MyClassInt
+    {
+        public int IntClass(int y, int x = 1)
+        {
+            return y + x;
+        }
+
+    }
+}
