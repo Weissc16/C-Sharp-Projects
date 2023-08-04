377da0fe51c1b91752b4aa414bb9a053fe989f0e
Cody Weiss <weissc16@gmail.com>
2023-08-04 09:34:57 -0600

    ClassMethod Assignment

---
+++ Basic_C#_Programs/ClassMethodAssignment/ClassMethodAssignment/DivideClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodAssignment
+{
+    class DivideClass
+    {
+        public void DivideNum(int num)
+        {
+            int x = num / 2;
+            Console.WriteLine(x);
+        }
+
+        public static void DivideNum(out int number, string divide)
+        {
+
+            number = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(number + divide);
+        }
+
+        
+        
+    }
+}
---
+++ Basic_C#_Programs/ClassMethodAssignment/ClassMethodAssignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number you would like divided by 2:");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            DivideClass userNum = new DivideClass();
+
+            userNum.DivideNum(x);
+            Console.ReadLine();
+
+            Console.WriteLine("Enter another number to divide by 2:");
+
+            DivideClass userNum2 = new DivideClass();
+
+
+            DivideClass.DivideNum(out int number, " is a great number");
+            Console.ReadLine();
+
+        }
+    }
+}
