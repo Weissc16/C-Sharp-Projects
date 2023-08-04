247a525b1cb49e37fe886469a0514e552cca6d2f
Cody Weiss <weissc16@gmail.com>
2023-08-04 10:23:19 -0600

    Employee Assignment

---
+++ Basic_C#_Programs/MethodAndObjectAssignment/MethodAndObjectAssignment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAndObjectAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee myObj = new Employee() { FirstName = "Sample ", LastName = "Student" };
+
+            myObj.SayName();
+            Console.ReadLine();
+        }
+    }
+}
