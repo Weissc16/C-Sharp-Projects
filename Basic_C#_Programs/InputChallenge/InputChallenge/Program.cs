84865eda6a1985f3920b823e457ac52a71a2a34b
Cody Weiss <weissc16@gmail.com>
2023-08-09 10:04:32 -0600

    Input Challenge
    using System.IO and StreamWriter, wrote the number to a text file.

---
+++ Basic_C#_Programs/InputChallenge/InputChallenge/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace InputChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input a number.");
+            string number = string.Format(Console.ReadLine());
+
+            Console.WriteLine("You chose the number " + number);
+            using (StreamWriter file = new StreamWriter(@"C:\Users\weiss\Logs\log.txt", true))
+            {
+                file.WriteLine(number);
+            }
+        }
+    }
+}
