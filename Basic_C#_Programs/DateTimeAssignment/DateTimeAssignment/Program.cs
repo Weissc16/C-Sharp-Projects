4d3ece1528478d4e12744742d99fffe28c22056f
Cody Weiss <weissc16@gmail.com>
2023-08-09 10:28:06 -0600

    DateTime Assignment

---
+++ Basic_C#_Programs/DateTimeAssignment/DateTimeAssignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now + ": Please enter a number.");
+            double number = Convert.ToDouble(Console.ReadLine());
+
+
+            DateTime dateTime = new DateTime();
+            dateTime = DateTime.Now;
+            Console.WriteLine("It will be " + (dateTime.AddHours(number)) + " in " + number + " hours.");
+
+            Console.ReadLine();
+
+        }
+    }
+}
